66e7528516939c8c99a0a30790b777e3fba52199
yuapX <shesterovmm@gmail.com>
2025-01-12 18:02:25 +0200

    dev

---
+++ src/dev/previews.jsx
@@ -0,0 +1,19 @@
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import {TypingText} from "@/shared/ui/TypingText/TypingText.jsx";
+import EyesChat from "@/widgets/components/EyesChat/EyesChat.jsx";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/TypingText">
+                <TypingText/>
+            </ComponentPreview>
+            <ComponentPreview path="/EyesChat">
+                <EyesChat/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
